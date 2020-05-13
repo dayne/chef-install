783f90d56be99ab6e483110e8993d1671ae967c2
Jason Field <jason@avon-lea.co.uk>
2020-05-13 18:45:35 +0100

    Speed up itterations

--- index.js
+++ index.js
@@ -12,43 +12,43 @@ async function main() {
     const omnitruckUrl = core.getInput('omnitruckUrl') || 'omnitruck.chef.io';
 
     // Turn on problem matchers
-    const matchersPath = path.join(__dirname, '..', 'problem-matchers');
+    const matchersPath = path.join(__dirname, 'problem-matchers');
     console.log(`##[add-matcher]${path.join(matchersPath, 'rubocop.json')}`);
 
     // This tool has intimate knowledge of the os
     // as Windows and Linux/MacOs run different installers
     // so we will check what OS and run appropriately
     // Create the args that the bash script will need
-    if (os.platform() != 'win32')
-    {
-      var channelParam = `-c ${channel}`
-      var projectParam = `-P ${project}`
-      if (version) {
-        versionParam = `-v ${version}`
-      }
-      else {
-        versionParam = ''
-      }
-      await exec.exec(`curl -L https://${omnitruckUrl}/install.sh -o chefDownload.sh`)
-      await exec.exec(`sudo chmod +x chefDownload.sh`)
-      await exec.exec(`sudo ./chefDownload.sh ${channelParam} ${projectParam} ${versionParam}`)
-      await exec.exec(`rm -f chefDownload.sh`)
-    }
-    // We are on windows so assume powershell
-    else
-    {
-      const windowsPath = core.getInput('windowsPath');
-      var channelParam = `-channel ${channel}`
-      var projectParam = `-project ${project}`
-      if (version) {
-        versionParam = `-version ${version}`
-      }
-      else {
-        versionParam = ''
-      }
-      await exec.exec(`powershell.exe -command ". { iwr -useb https://${omnitruckUrl}/install.ps1 } | iex; install ${channelParam} ${projectParam} ${versionParam}"`)
-      core.addPath(`${windowsPath}\\bin`)
-    }
+    // if (os.platform() != 'win32')
+    // {
+    //   var channelParam = `-c ${channel}`
+    //   var projectParam = `-P ${project}`
+    //   if (version) {
+    //     versionParam = `-v ${version}`
+    //   }
+    //   else {
+    //     versionParam = ''
+    //   }
+    //   await exec.exec(`curl -L https://${omnitruckUrl}/install.sh -o chefDownload.sh`)
+    //   await exec.exec(`sudo chmod +x chefDownload.sh`)
+    //   await exec.exec(`sudo ./chefDownload.sh ${channelParam} ${projectParam} ${versionParam}`)
+    //   await exec.exec(`rm -f chefDownload.sh`)
+    // }
+    // // We are on windows so assume powershell
+    // else
+    // {
+    //   const windowsPath = core.getInput('windowsPath');
+    //   var channelParam = `-channel ${channel}`
+    //   var projectParam = `-project ${project}`
+    //   if (version) {
+    //     versionParam = `-version ${version}`
+    //   }
+    //   else {
+    //     versionParam = ''
+    //   }
+    //   await exec.exec(`powershell.exe -command ". { iwr -useb https://${omnitruckUrl}/install.ps1 } | iex; install ${channelParam} ${projectParam} ${versionParam}"`)
+    //   core.addPath(`${windowsPath}\\bin`)
+    // }
 
   } catch (error){
     core.setFailed(error.message);
